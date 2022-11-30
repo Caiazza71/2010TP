c18fc7654842fc7c824ef66493565dece81e4678
tcarlson03 <tcarlson2021@my.fit.edu>
2022-11-29 19:20:13 -0500

    Update Trie.java

--- Trie.java
+++ Trie.java
@@ -181,6 +181,7 @@ public class Trie
         Trie prefixTree;
         prefixTree = new Trie();  
         Scanner scan = new Scanner(new File(args[0]));
+        Scanner scans = new Scanner(new File(args[1]));
         Scanner scanner = new Scanner(System.in);
 
         while(scan.hasNext()) {
